c44d7101b8d99421faea670b07362a00748598c7
Mike Allanson <michael.allanson@gmail.com>
2019-08-24 10:36:03 +0100

    Mock useQuery results

--- src/components/__tests__/index.js
+++ src/components/__tests__/index.js
@@ -1,11 +1,38 @@
-import React from "react";
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 import renderer from "react-test-renderer"
 
 import Bio from "../bio"
+import { useStaticQuery } from "gatsby"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Bio />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+beforeEach(() => {
+  useStaticQuery.mockImplementationOnce(() => {
+    return {
+      avatar: {
+        childImageSharp: {
+          fixed: {
+            base64: "foo",
+            width: 1,
+            height: 1,
+            src: "foo",
+            srcSet: "foo",
+          },
+        },
+      },
+      site: {
+        siteMetadata: {
+          author: `Mock author`,
+          social: {
+            twitter: "mockhandle",
+          },
+        },
+      },
+    }
+  })
+})
+
+it("renders without crashing", () => {
+  const div = document.createElement("div")
+  ReactDOM.render(<Bio />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
