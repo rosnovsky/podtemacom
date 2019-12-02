f23706769d56eacdc342ed1ee7ee2cb79b670a7d
Artem Rosnovsky <artem@rosnovsky.us>
2019-12-01 21:03:13 -0800

    debugging deployment

--- web/src/auth.js
+++ web/src/auth.js
@@ -7,7 +7,7 @@ const auth = isBrowser
     ? new auth0.WebAuth({
         domain: "rosnovsky.auth0.com",
         clientID: '56CVFXGTxGzffHxOxwGsQxoXQoirrgdC',
-        redirectUri: 'http://web.rosnovsky.now.sh',
+        redirectUri: 'https://web.rosnovsky.now.sh',
         responseType: "token id_token",
         scope: "openid profile email",
         })
